a827a11188c6bd40723b1abffab4b8bdc1edf63a
Md.Saddam Hossain <csesolve6548@gmail.com>
2020-02-02 17:01:54 +0600

    Execute Without Main Method

---
+++ src/ExecuteWithoutMainMethod/ExecuteWithoutMainMethod.java
@@ -0,0 +1,10 @@
+
+package ExecuteWithoutMainMethod;
+
+public class ExecuteWithoutMainMethod {
+    
+    static{
+        System.out.println("Static Block is invoked!!!");
+        System.exit(0);
+    }
+}
